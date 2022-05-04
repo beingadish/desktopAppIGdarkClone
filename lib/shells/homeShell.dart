eedbf16997b05dddc96eace0b72667e3bcc766d2
Aadarsh Pandey <aadarsh2012014@akgec.ac.in>
2022-05-04 21:20:12 +0530

    errorEdit

--- lib/shells/homeShell.dart
+++ lib/shells/homeShell.dart
@@ -2,6 +2,7 @@ import 'package:fluent_ui/fluent_ui.dart';
 import 'package:flutter/material.dart' as material;
 import 'package:google_fonts/google_fonts.dart';
 import '../models/colorThemes.dart';
+import 'package:flutter_advanced_avatar/flutter_advanced_avatar.dart';
 
 class HomeShell extends StatefulWidget {
   const HomeShell({Key? key}) : super(key: key);
@@ -15,24 +16,31 @@ var darkColors = DarkThemeScheme();
 Padding heading(String topicName) {
   return Padding(
     padding: const EdgeInsets.only(top: 10, bottom: 20.0, left: 20.0),
-    child: Builder(
-        builder: (context) {
-          return Text(
-            topicName,
-            style: GoogleFonts.oleoScript(
-              fontSize: MediaQuery.of(context).size.width*0.05,
-              color: darkColors.heading,
-            ),
-          );
-        }
-    ),
+    child: Builder(builder: (context) {
+      return Text(
+        topicName,
+        style: GoogleFonts.oleoScript(
+          fontSize: MediaQuery.of(context).size.width *
+              0.005 *
+              (MediaQuery.of(context).size.height) *
+              0.009,
+          color: darkColors.heading,
+        ),
+      );
+    }),
   );
 }
 
-material.CircleAvatar story(String heading, String content, String imageLoc) {
-  return material.CircleAvatar(
-    backgroundImage: ,
-  );
+Builder story() {
+  return Builder(builder: (context) {
+    return AdvancedAvatar(
+      size: (MediaQuery.of(context).size.width) *
+          (MediaQuery.of(context).size.height) *
+          (0.00015),
+      name: "img1",
+      image: const material.AssetImage("asset/images/DP.jpeg"),
+    );
+  });
 }
 
 SizedBox hSpace() {
@@ -47,77 +55,50 @@ SizedBox vSpace() {
   );
 }
 
-SingleChildScrollView buildTileRow(
-    String h1,
-    String h2,
-    String h3,
-    String h4,
-    String c1,
-    String c2,
-    String c3,
-    String c4,
-    String l1,
-    String l2,
-    String l3,
-    String l4) {
-  return SingleChildScrollView(
-    scrollDirection: Axis.horizontal,
-    child: Row(
-      children: [
-        buildTile(h1, c1, l1),
-        hSpace(),
-        buildTile(h2, c2, l2),
-        hSpace(),
-        buildTile(h3, c3, l3),
-        hSpace(),
-        buildTile(h4, c4, l4),
-        hSpace(),
-        hSpace(),
-        Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            material.ElevatedButton(
-              onPressed: () {},
-              child: const Center(child: Icon(material.Icons.arrow_forward_outlined)),
-              style: material.ElevatedButton.styleFrom(
-                padding: const EdgeInsets.only(bottom: 20.0),
-                primary: Colors.black,
-              ),
-            ),
-            const Text(
-              "See More !",
-              style: TextStyle(
-                color: material.Colors.white60,
-                fontSize: 18,
-              ),
-            ),
-          ],
-        ),
-        hSpace(),
-      ],
-    ),
+ScaffoldPage storyRow() {
+  return ScaffoldPage.scrollable(
+    children: [
+      hSpace(),
+      hSpace(),
+      story(),
+      hSpace(),
+      hSpace(),
+      story(),
+      hSpace(),
+      hSpace(),
+      story(),
+      hSpace(),
+      hSpace(),
+      story(),
+      hSpace(),
+      hSpace(),
+      story(),
+      hSpace(),
+      hSpace(),
+      story(),
+      hSpace(),
+      hSpace(),
+      story(),
+    ],
+
   );
 }
 
 class _HomeShellState extends State<HomeShell> {
   @override
   Widget build(BuildContext context) {
-    return material.Material(
-      child: material.Scaffold(
-        backgroundColor: material.Colors.blueGrey,
-        body: Builder(
-          builder: (context) {
-            return Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                heading("Welcome !"),
-                vSpace(),
-              ],
-            );
-          },
-        ),
-      ),
-    );
+    return ScaffoldPage.scrollable(
+        // backgroundColor: material.Colors.blueGrey.shade700,
+        children: [
+          Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              heading("Welcome !"),
+              vSpace(),
+              storyRow(),
+            ],
+          )
+        ]);
   }
 }
